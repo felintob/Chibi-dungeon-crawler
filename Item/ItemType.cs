dc9d7ad9e025c9aacbadaa6069a6bd5892d15b09
Bruno Felinto <bfelinto@gmail.com>
2025-02-07 15:49:10 -0300

    Add files via upload

---
+++ Item/ItemType.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Item{
+    public enum ItemType
+    {
+        Air,
+        Key,
+        Potion,
+        BossKey,
+        Sword
+    }
+
+}
